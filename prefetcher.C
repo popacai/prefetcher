42c36dd9de2fb51c26ce696b8a631800d22bd203
tao <popacai@gmail.com>
2014-05-16 15:37:20 -0700

    Trace

--- prefetcher.C
+++ prefetcher.C
@@ -10,17 +10,56 @@
 
 #include "prefetcher.h"
 #include <stdio.h>
+#include <memory.h>
+
 
 Prefetcher::Prefetcher() {
+    int i;
+    for (i = 0; i < MAX_QUEUE_SIZE; i++){
+        memset(&(p_queue[i]), 0, sizeof(p_item_t));
+    }
+    previous_pc = 0;
+    previous_addr = 0;
+
     _ready = false; 
 }
+
+int Prefetcher::find_pc(u_int32_t pc) {
+    p_item_t* p;
+    int free_p;
+    free_p = 0;
+    int i;
+
+    int min_count;
+    min_count = 1;
+    for (i = 0; i < MAX_QUEUE_SIZE; i++){
+        p = &(p_queue[i]);
+        if (p->pc_addr == pc){
+            return i;
+        }
+        if (p->count < min_count){
+            free_p = i;
+            min_count = p->count;
+        }
+    }
+    return free_p;
+}
+
+void Prefetcher::decrease_count() {
+    int i;
+    p_item_t* p;
+    for (i = 0; i < MAX_QUEUE_SIZE; i++) {
+        p = &(p_queue[i]);
+        p->count--;
+    }
+}
+
 
 bool Prefetcher::hasRequest(u_int32_t cycle) { 
     return _ready; 
 }
 
 Request Prefetcher::getRequest(u_int32_t cycle) { 
-    printf("%u\n", cycle);
     return _nextReq; 
 }
 
@@ -41,9 +80,47 @@ void Prefetcher::cpuRequest(Request req) {
         printf("HitL2=%u\n", req.HitL2);
     }
     */
+    //add the pc
+
+    //if (!_ready) {
+    _nextReq.addr = req.addr + 16;
+    _ready = true;
+
+    
+    /*
+    int pos;
+    u_int32_t offset;
+    p_item_t* p;
+    pos = find_pc(previous_pc);
+    p = &(p_queue[pos]);
+    {
+        if ((p->pc_addr == req.addr)) {
+            _nextReq.addr = req.addr + p->offset;
+            _ready = true;
+            p->total++;
+        }else{
+            _nextReq.addr = req.addr + 16;
+            _ready = true;
+        }
+    }
+
+    //If hit the p->prediction
+    p->pc_addr = previous_pc;
+    p->count = MAX_QUEUE_SIZE; //init value
+    p->offset = req.addr - previous_addr;
+    p->offset = p->offset < 16 ? 16 : p->offset;
+
+
+    previous_pc = req.pc;
+    previous_addr = req.addr;
+
+    this->decrease_count();
+    */
+
+    /*
 	if(!_ready && !req.HitL1) {
 		_nextReq.addr = req.addr + 16;
 		_ready = true;
 	}
-    _ready = false;
+    */
 }
--- prefetcher.h
+++ prefetcher.h
@@ -6,19 +6,47 @@
  *
  */
 
+
 #ifndef PREFETCHER_H
 #define PREFETCHER_H
+
+#define MAX_QUEUE_SIZE 2000
 
 #include <sys/types.h>
 #include "mem-sim.h"
 
+typedef struct p_item {
+    u_int32_t pc_addr;
+    u_int32_t offset;
+    int count;
+
+    int hit;
+    int total;
+
+}p_item_t;
 class Prefetcher {
   private:
 	bool _ready;
 	Request _nextReq;
+    //
+    //
+    //PC based prefetcher
+    //PC_predicter["PC"] = [next addr's offset]
+    //
+    //Try to fill it with
+    //  previous_pc   = ?
+    //  previous_addr = ?
+    u_int32_t previous_pc;
+    u_int32_t previous_addr;
+
+    //Fill the map?
+    p_item_t p_queue[MAX_QUEUE_SIZE];
 
   public:
 	Prefetcher();
+
+    int find_pc(u_int32_t pc);
+    void decrease_count();
 
 	// should return true if a request is ready for this cycle
 	bool hasRequest(u_int32_t cycle);
